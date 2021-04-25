9b5075e9eb7a8f7b6988aed25c4d8b41cbeb00ba
Bartosz Slesar <bartoszslesar@gmail.com>
2021-04-25 14:09:44 +0200

    Check multiple word palindrom

--- palindroms/palindrom_slicing.py
+++ palindroms/palindrom_slicing.py
@@ -1,7 +1,12 @@
+import re
+
+
 def is_palindrom(arg):
     """ function check if provided string or integer is a palindrom using slicing"""
+    separator = re.compile(r"[,'\s.\"?]+")
     if isinstance(arg, str) or isinstance(arg, int):
         pal = str(arg).lower()
+        pal = separator.sub("", pal)
         result = False
         if pal == pal[len(pal)::-1]:
             print(f"{arg} is a palindrom")
--- test/palindrom_test/test_is_palindrom.py
+++ test/palindrom_test/test_is_palindrom.py
@@ -4,8 +4,20 @@ from palindroms.palindrom_slicing import is_palindrom
 
 class isPalindromTest(unittest.TestCase):
 
-    def test_short_palindrom(self):
-        self.assertTrue(is_palindrom("Anna"), "Anna is a palindrom")
-        self.assertTrue(is_palindrom("Kayak"), "Kayak is a palindrom")
-        self.assertTrue(is_palindrom("Level"), "Level is a palindrom")
-        self.assertTrue(is_palindrom("Level"), "Level is a palindrom")
+    def test_palindrom_True(self):
+        self.assertTrue(is_palindrom(545))
+        self.assertTrue(is_palindrom("Kayak"))
+        self.assertTrue(is_palindrom(121))
+        self.assertTrue(is_palindrom("Level"))
+
+    def test_palindrom_False(self):
+        self.assertFalse(is_palindrom("test"))
+        self.assertFalse(is_palindrom(123))
+        self.assertFalse(is_palindrom("google"))
+        self.assertFalse(is_palindrom(332))
+
+    def test_multiple_word_palindrom_True(self):
+        self.assertTrue(is_palindrom("Don't nod."))
+        self.assertTrue(is_palindrom("Red rum, sir, is murder"))
+        self.assertTrue(is_palindrom("Eva, can I see bees in a cave?"))
+        self.assertTrue(is_palindrom("No lemon, no melon"))
