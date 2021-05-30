44073ffd8c49c9dc190a88070e37656be7b5da28
Bartosz Slesar <bartoszslesar@gmail.com>
2021-05-30 21:51:22 +0200

    coding exercise Python master class

---
+++ flower_shrub/flower_shrub.py
@@ -0,0 +1,31 @@
+data = [
+    "Andromeda - Shrub",
+    "Bellflower - Flower",
+    "China Pink - Flower",
+    "Daffodil - Flower",
+    "Evening Primrose - Flower",
+    "French Marigold - Flower",
+    "Hydrangea - Shrub",
+    "Iris - Flower",
+    "Japanese Camellia - Shrub",
+    "Lavender - Shrub",
+    "Lilac - Shrub",
+    "Magnolia - Shrub",
+    "Peony - Shrub",
+    "Queen Anne's Lace - Flower",
+    "Red Hot Poker - Flower",
+    "Snapdragon - Flower",
+    "Sunflower - Flower",
+    "Tiger Lily - Flower",
+    "Witch Hazel - Shrub",
+]
+
+flowers = []
+shrubs = []
+
+for val in data:
+    plant = val.split("-")[0].strip()
+    if "flower" in val.casefold():
+        flowers.append(plant)
+    else:
+        shrubs.append(plant)
