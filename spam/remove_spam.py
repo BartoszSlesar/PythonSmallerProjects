6235ccfeb6f7aaecfc48212e95828f2c58e1f1cc
Bartosz Slesar <bartoszslesar@gmail.com>
2021-05-31 23:54:26 +0200

    added join example in remove_spam

---
+++ spam/remove_spam.py
@@ -0,0 +1,20 @@
+menu = [
+    ["egg", "bacon"],
+    ["egg", "sausage", "bacon"],
+    ["egg", "spam"],
+    ["egg", "bacon", "spam"],
+    ["egg", "bacon", "sausage", "spam"],
+    ["spam", "bacon", "sausage", "spam"],
+    ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam"],
+    ["spam", "egg", "spam", "spam", "bacon", "spam"],
+]
+
+#using generator and loop, to remove spam from menu, printed separated by comma
+for data in menu:
+    removed_spam = ", ".join((item for item in data if item != "spam"))
+    print(removed_spam)
+
+#join with list
+data = ["dog", "rabbit", "snake", "mouse", "cat"]
+result = ", ".join(data)
+print(result)
