d85bd24141e950e57d7fa638dfd80f152125d631
Bartosz Slesar <bartoszslesar@gmail.com>
2021-06-06 18:22:14 +0200

    added Docstring and type hint to is_palindrome function

--- palindroms/palindrom_slicing.py
+++ palindroms/palindrom_slicing.py
@@ -1,20 +1,31 @@
 import re
+from typing import Union
 
 
-def is_palindrom(arg):
-    """ function check if provided string or integer is a palindrom using slicing"""
-    #regex to filter separators when multi text is passed
+def is_palindrome(__arg: Union[int, str]) -> bool:
+    """check if provided `arg` is palindrome.
+
+
+    palindrome is word, phrase, or sequence that reads the same backwards as forwards.
+
+
+    :param __arg: is a positional only argument of Type`string` or `int` that will be checked if it's a palindrome
+        Sentences also ca be passed.
+    :raises ValueError: when `arg` argument is not a `string` or `int` exception is raised.
+    :return bool: function returns `True` if `arg` is palindrome `False` otherwise.
+    """
+    # regex to filter separators when multi text is passed
     separator = re.compile(r"[,'\s.\"?!]+")
-    if isinstance(arg, str) or isinstance(arg, int):
-        pal = str(arg).lower()
+    if isinstance(__arg, str) or isinstance(__arg, int):
+        pal = str(__arg).lower()
         # filtering all text separators
         pal = separator.sub("", pal)
         result = False
         if pal == pal[len(pal)::-1]:
-            print(f"{arg} is a palindrom")
+            print(f"{__arg} is a palindrom")
             result = True
         else:
-            print(f"{arg} is not a palindrom")
+            print(f"{__arg} is not a palindrom")
         return result
     else:
         raise ValueError("Provided argument is not int or string")
--- test/palindrom_test/test_is_palindrom.py
+++ test/palindrom_test/test_is_palindrom.py
@@ -1,44 +1,44 @@
 import unittest
-from palindroms.palindrom_slicing import is_palindrom
+from palindroms.palindrom_slicing import is_palindrome
 
 
 class isPalindromTest(unittest.TestCase):
 
     def test_palindrom_True(self):
-        self.assertTrue(is_palindrom(545))
-        self.assertTrue(is_palindrom("Kayak"))
-        self.assertTrue(is_palindrom(121))
-        self.assertTrue(is_palindrom("Level"))
+        self.assertTrue(is_palindrome(545))
+        self.assertTrue(is_palindrome("Kayak"))
+        self.assertTrue(is_palindrome(121))
+        self.assertTrue(is_palindrome("Level"))
 
     def test_palindrom_False(self):
-        self.assertFalse(is_palindrom("test"))
-        self.assertFalse(is_palindrom(123))
-        self.assertFalse(is_palindrom("google"))
-        self.assertFalse(is_palindrom(332))
+        self.assertFalse(is_palindrome("test"))
+        self.assertFalse(is_palindrome(123))
+        self.assertFalse(is_palindrome("google"))
+        self.assertFalse(is_palindrome(332))
 
     def test_multiple_word_palindrom_True(self):
-        self.assertTrue(is_palindrom("Don't nod."))
-        self.assertTrue(is_palindrom("Red rum, sir, is murder"))
-        self.assertTrue(is_palindrom("Eva, can I see bees in a cave?"))
-        self.assertTrue(is_palindrom("No lemon, no melon"))
+        self.assertTrue(is_palindrome("Don't nod."))
+        self.assertTrue(is_palindrome("Red rum, sir, is murder"))
+        self.assertTrue(is_palindrome("Eva, can I see bees in a cave?"))
+        self.assertTrue(is_palindrome("No lemon, no melon"))
 
     def test_multiple_word_palindrom_False(self):
-        self.assertFalse(is_palindrom("Some unnecessary sentence."))
-        self.assertFalse(is_palindrom("This, Sentence is not a Palindrom."))
-        self.assertFalse(is_palindrom("Is this sentence a Palindrom?"))
-        self.assertFalse(is_palindrom("This sentece Also is not a Palindrom!!!!"))
+        self.assertFalse(is_palindrome("Some unnecessary sentence."))
+        self.assertFalse(is_palindrome("This, Sentence is not a Palindrom."))
+        self.assertFalse(is_palindrome("Is this sentence a Palindrom?"))
+        self.assertFalse(is_palindrome("This sentece Also is not a Palindrom!!!!"))
 
 
     def test_value_Error_when_incorrect_value_is_provided(self):
         with self.assertRaises(ValueError) as context:
-            is_palindrom([1, 2, 3, 4, 5, 6])
+            is_palindrome([1, 2, 3, 4, 5, 6])
         self.assertEqual(str(context.exception), "Provided argument is not int or string")
         with self.assertRaises(ValueError) as context:
-            is_palindrom({1: 4, "test": "no"})
+            is_palindrome({1: 4, "test": "no"})
         self.assertEqual(str(context.exception), "Provided argument is not int or string")
         with self.assertRaises(ValueError) as context:
-            is_palindrom((1, 2, 5, 223, 565))
+            is_palindrome((1, 2, 5, 223, 565))
         self.assertEqual(str(context.exception), "Provided argument is not int or string")
         with self.assertRaises(ValueError) as context:
-            is_palindrom({1, 2, 3, 45, 5})
+            is_palindrome({1, 2, 3, 45, 5})
         self.assertEqual(str(context.exception), "Provided argument is not int or string")
