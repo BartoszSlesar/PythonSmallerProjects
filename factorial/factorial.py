c85890244000971133ab71b75374403a02872815
Bartosz Slesar <bartoszslesar@gmail.com>
2021-06-07 20:11:07 +0200

    added factorial function and unit test

---
+++ factorial/factorial.py
@@ -0,0 +1,14 @@
+def factorial(n: int) -> int:
+    """
+    return factorial of number `n`
+
+    Function that will calculate factorial of number `n` passed as argument (`n`!)
+
+    :param n: its an `int` passed as argument to factorial function
+
+    :returns: return factorial of number `n`
+    """
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
